a99841792aea01aadbfb18a476e8d590dad00a1e
nadia <nadia.ardhianie@act.id>
2021-10-28 14:39:32 +0700

    first commit retsuna

---
+++ resources/views/user/katalog.blade.php
@@ -0,0 +1,54 @@
+@include ('layouts.navbar')
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+    <div class="card mb-3" style="width: 18rem;">
+        <div class="card-header">
+            Kategori Produk
+        </div>
+        <div class="list-group">
+            <a href="#" class="list-group-item list-group-item-action">First item</a>
+            <a href="#" class="list-group-item list-group-item-action">Second item</a>
+            <a href="#" class="list-group-item list-group-item-action">Third item</a>
+        </div> 
+    </div>
+    <div class="container" style="width: 18rem;">
+        <div class="row g-0">
+            <div class="col-md-9">  
+                <h1> Test</h1>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="container">
+        <div class="row g-0">
+            <div class="col-md-3">
+                <img class="w-500" src="https://placeimg.com/150/150/nature" alt="gambar alam">
+            </div>
+            <div class="col-md-3">
+                <img class="w-500" src="https://placeimg.com/150/150/nature" alt="gambar alam">
+            </div>
+            <div class="col-md-3">
+                <img class="w-500" src="https://placeimg.com/150/150/nature" alt="gambar alam">
+            </div>
+            <div class="col-md-3">
+                <img class="w-500" src="https://placeimg.com/150/150/nature" alt="gambar alam">
+            </div>
+            <div class="col-md-3">
+                <img class="w-500" src="https://placeimg.com/150/150/nature" alt="gambar alam">
+            </div>
+            <div class="col-md-3">
+                <img class="w-500" src="https://placeimg.com/150/150/nature" alt="gambar alam">
+            </div>
+            <div class="col-md-3">
+                <img class="w-500" src="https://placeimg.com/150/150/nature" alt="gambar alam">
+            </div>
+            <div class="col-md-3">
+                <img class="w-500" src="https://placeimg.com/150/150/nature" alt="gambar alam">
+            </div>
+        </div>
+    </div> -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,46 @@
+@include ('layouts.navbar')
+<body>
+    <div class="container">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+                <li data-target="#myCarousel" data-slide-to="4"></li>
+            </ol>
+            
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="https://image.indonetwork.co.id/f-webp/banner_company_images/thumbs/1400x600/2021/01/19/c0a6e085cab61b6bf0c1606e8c2bd9d7.jpg" class="img-fluid" alt="PT Zahra Inti Retsuna">
+                </div>
+                <div class="item">
+                    <img src="https://image.indonetwork.co.id/f-webp/banner_company_images/thumbs/1400x600/2021/07/26/1bbbca3d729f944af03f81da46d491d8.jpg" class="img-fluid" alt="PT Zahra Inti Retsuna">
+                </div>
+                <div class="item">
+                    <img src="https://image.indonetwork.co.id/f-webp/banner_company_images/thumbs/1400x600/2021/01/19/e5aa17226a5ee7d211a599ced99042cb.jpg" class="img-fluid" alt="PT Zahra Inti Retsuna">
+                </div>
+                <div class="item">
+                    <img src="https://image.indonetwork.co.id/f-webp/banner_company_images/thumbs/1400x600/2021/01/19/159e115aa51531bdb8dff1abbe86b189.jpg" class="img-fluid" alt="PT Zahra Inti Retsuna">
+                </div>
+                <div class="item">
+                    <img src="https://image.indonetwork.co.id/f-webp/banner_company_images/thumbs/1400x600/2021/07/26/0234e79d8a32b49c8efb7160c88f4842.jpg" class="img-fluid" alt="PT Zahra Inti Retsuna">
+                </div>
+            </div>
+            
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
